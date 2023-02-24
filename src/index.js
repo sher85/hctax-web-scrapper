57fb485f6335dc93fb2f4e0f77ffa283acbd0557
sher85 <32082005+sher85@users.noreply.github.com>
2023-02-23 23:43:58 -0600

    added ids to each listing

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ request(url, (error, response, html) => {
     $(container + ' > li').each((i, el) => {
       // Extract the data for the current listing
       const property = {
+        propertyID: i,
         address: $(el).find('h3 > span.address').text(),
         city: $(el).find('h3 > span.city').text(),
         state: $(el).find('h3 > span.state').text(),
@@ -41,13 +42,8 @@ request(url, (error, response, html) => {
         minBid: $(el)
           .find('table tbody tr:nth-child(2) td.minBid.listingTd strong')
           .text(),
+        additionalField: $(el).find('span > strong').text(),
       };
-
-      // Check whether the additional field exists for this listing
-      const additionalField = $(el).find('span > strong').text();
-      if (additionalField) {
-        property.additionalField = additionalField;
-      }
 
       // Add the property object to the properties array
       properties.push(property);
