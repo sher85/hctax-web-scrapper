adbec6da7b5f390ca62af24ee052df2a8b859bef
Mauricio Castro <32082005+sher85@users.noreply.github.com>
2026-03-28 02:35:44 -0500

    chore: ignore narrow dependabot bump format

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0/'
+  helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0/',
+  ignores: [
+    (message = '') =>
+      message.startsWith('Bump ') &&
+      message.includes('updated-dependencies:') &&
+      message.includes('dependabot[bot]')
+  ]
 };
